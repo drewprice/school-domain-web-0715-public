ba60f0d7646037ec9bb674dc7aab3bda9c64387e
Drew Price <drewprice@users.noreply.github.com>
2015-08-05 10:52:08 -0400

    Add solution

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# Write code here
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name   = name
+    @roster = {}
+  end
+
+  def add_student(student, level)
+    if @roster[level]
+      @roster[level] << student
+    else
+      @roster[level] = [student]
+    end
+  end
+
+  def grade(level)
+    @roster[level]
+  end
+
+  def sort
+    @roster.each { |level, students| @roster[level] = students.sort }
+  end
+end
